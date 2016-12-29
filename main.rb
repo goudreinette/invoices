c14cb4e5fc56e26072a9c1432525f1364103e417
reinvdwoerd <reinvanderwoerd@me.com>
2016-12-29 23:52:52 +0100

    items back

--- main.rb
+++ main.rb
@@ -6,4 +6,6 @@ Invoice.new_for_client 'Hans van der Woerd' do
   address  "Korte Smeden 8g"
   postcode "8011VC"
   city     "Zwolle"
+  items    'Add To Cart' => 150,
+           'WooEvents'   => 450
 end.to_pdf
